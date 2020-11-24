2b7a486342701366269cec812cdcb1dac29c1c7c
itzomen <peng.akebuon2468@gmail.com>
2020-11-24 13:08:06 +0100

    added meta class to Category model

--- django/quiz/models.py
+++ django/quiz/models.py
@@ -5,6 +5,10 @@ class Category(models.Model):
 
     name = models.CharField(max_length = 250)
 
+    class Meta:
+        verbose_name = _("Category")
+        verbose_name_plural = _("Categories")
+
     def __str__(self):
         return self.name
     
